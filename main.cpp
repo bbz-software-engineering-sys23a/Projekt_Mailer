d2c5cd46bf4970e1e1103ca3d29c30d57a5fabb8
Latrusanimi <stephan.fankhauser@bbz-sh.ch>
2025-04-14 16:05:51 +0200

    Pseudo Typen erstellt

--- main.cpp
+++ main.cpp
@@ -9,10 +9,20 @@ int main()
 {
     SetConsoleOutputCP(CP_UTF8);
     bool x = true;
+    int mitgliedernummer = 0;
+    string vorname;
+    string nachname;
+    int plz;
+    string wohnohrt;
+    string mail;
+    string telefon;
+    string typ;
+
     while (x == true) {
 
         int auswahl;
-        cout << "Guten Tag. Was möchten sie tun? \n 1) Daten eingeben \n 2) Daten anzeigen \n 3) Das Programm beenden" << endl;
+        cout << "Guten Tag. Was möchten sie tun? \n 1) Daten eingeben \n 2) Daten anzeigen \n 3) Ein Mail versenden \n 4) Das Programm beenden" << endl;
+        cout << mitgliedernummer << endl;
         cin >> auswahl;
         switch (auswahl) {
             case 1:
@@ -22,6 +32,9 @@ int main()
                 cout << "Sie haben Daten anzeigen gewählt" << endl;
             break;
             case 3:
+                cout << "Sie haben Email versenden gewählt. \nAn wenn möchten sie ein Mail versenden?" << endl;
+            break;
+            case 4:
                 x = false;
             break;
         }
