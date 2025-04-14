e04e61e631ed1d9fbfd60dd00c28a29268b8e3a2
pesche70 <Peter.Meier@bbz-sh.ch>
2025-04-14 15:43:00 +0200

    1.versuch-csv schreiben

--- cmake-build-debug/CMakeFiles/Projekt_Mailer.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Projekt_Mailer.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/steph/OneDrive - Berufsbildungszentrum Schaffhausen/Informatik/CLionProjects/Projekt_2025/Projekt_Mailer/main.cpp" "CMakeFiles/Projekt_Mailer.dir/main.cpp.obj" "gcc" "CMakeFiles/Projekt_Mailer.dir/main.cpp.obj.d"
+  "D:/HF-SH/Software/Projektwoche/Projekt_Mailer/main.cpp" "CMakeFiles/Projekt_Mailer.dir/main.cpp.obj" "gcc" "CMakeFiles/Projekt_Mailer.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
+
+    void csv_speichern() {
+
+
+
+        std::ofstream file("output.csv");
+        std::vector<std::vector<std::string>> data = {
+            {"Name", "Alter", "Stadt"},
+            {"John Doe", "29", "New York"},
+            {"Jane Smith", "34", "Los Angeles"}
+        };
+
+        for (const auto& row : data) {
+            for (size_t i = 0; i < row.size(); i++) {
+                file << row[i];
+                if (i < row.size() - 1) file << ",";
+            }
+            file << "\n";
+
+
+            return 0;
+        }
+    }
+    ;*/
