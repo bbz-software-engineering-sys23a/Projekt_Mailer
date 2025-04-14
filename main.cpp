7be942ba75d6f3976acff8262ea7d4f90e3a8afa
Latrusanimi <stephan.fankhauser@bbz-sh.ch>
2025-04-14 15:33:41 +0200

    Hauptschleife Auswahl erstellt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    bool x = true;
+    while (x == true) {
+
+        int auswahl;
+        cout << "Guten Tag. Was möchten sie tun? \n 1) Daten eingeben \n 2) Daten anzeigen \n 3) Das Programm beenden" << endl;
+        cin >> auswahl;
+        switch (auswahl) {
+            case 1:
+                cout << "Sie haben Daten eingeben gewählt" << endl;
+            break;
+            case 2:
+                cout << "Sie haben Daten anzeigen gewählt" << endl;
+            break;
+            case 3:
+                x = false;
+            break;
+        }
+    }
     return 0;
 }
