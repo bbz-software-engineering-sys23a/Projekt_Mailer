40a7ca1f84464089b368df5db4147682ba5c719d
Latrusanimi <stephan.fankhauser@bbz-sh.ch>
2025-04-15 09:44:09 +0200

    Debugging und Funktionen hinzugefügt

--- main.cpp
+++ main.cpp
@@ -20,30 +20,38 @@ int main()
     string telefon;
     string typ;
 
+    void datenErfassen();
+    void datenAusgeben();
+    void mailVersenden();
+
     while (x == true) {
         string eingabe;
         int auswahl;
         cout << "Guten Tag. Was möchten sie tun? \n 1) Daten eingeben \n 2) Daten anzeigen \n 3) Ein Mail versenden \n 4) Das Programm beenden\n" << endl;
-        cout << mitgliedernummer << endl;
         cin >> eingabe;
 
         try {
             auswahl = stoi(eingabe);
             } catch(const invalid_argument&) {
                 cout << "Ungültige Eingabe\n" << endl;
+                continue;
             } catch(const out_of_range&) {
                 cout << "Ungültige Eingabe\n" << endl;
+                continue;
             }
 
         switch (auswahl) {
             case 1:
                 cout << "Sie haben Daten eingeben gewählt\n" << endl;
+                datenErfassen();
             break;
             case 2:
                 cout << "Sie haben Daten anzeigen gewählt\n" << endl;
+                datenAusgeben();
             break;
             case 3:
                 cout << "Sie haben Email versenden gewählt. \nAn wenn möchten sie ein Mail versenden?\n" << endl;
+                mailVersenden();
             break;
             case 4:
                 x = false;
@@ -54,4 +62,22 @@ int main()
         }
     }
     return 0;
+}
+
+
+
+void datenErfassen() {
+    cout << "Daten Erfassen\n" << endl;
+}
+
+
+
+void datenAusgeben() {
+    cout << "Daten Ausgeben\n" << endl;
+}
+
+
+
+void mailVersenden() {
+    cout << "Mail Versenden\n" << endl;
 }
