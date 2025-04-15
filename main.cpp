f790e577f234ce504bbe32c2c2ef11a84d8daf76
Latrusanimi <stephan.fankhauser@bbz-sh.ch>
2025-04-15 08:46:35 +0200

    Neu erarbeitet

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 
 
+
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
+
     bool x = true;
     int mitgliedernummer = 0;
     string vorname;
@@ -19,23 +21,35 @@ int main()
     string typ;
 
     while (x == true) {
-
+        string eingabe;
         int auswahl;
-        cout << "Guten Tag. Was möchten sie tun? \n 1) Daten eingeben \n 2) Daten anzeigen \n 3) Ein Mail versenden \n 4) Das Programm beenden" << endl;
+        cout << "Guten Tag. Was möchten sie tun? \n 1) Daten eingeben \n 2) Daten anzeigen \n 3) Ein Mail versenden \n 4) Das Programm beenden\n" << endl;
         cout << mitgliedernummer << endl;
-        cin >> auswahl;
+        cin >> eingabe;
+
+        try {
+            auswahl = stoi(eingabe);
+            } catch(const invalid_argument&) {
+                cout << "Ungültige Eingabe\n" << endl;
+            } catch(const out_of_range&) {
+                cout << "Ungültige Eingabe\n" << endl;
+            }
+
         switch (auswahl) {
             case 1:
-                cout << "Sie haben Daten eingeben gewählt" << endl;
+                cout << "Sie haben Daten eingeben gewählt\n" << endl;
             break;
             case 2:
-                cout << "Sie haben Daten anzeigen gewählt" << endl;
+                cout << "Sie haben Daten anzeigen gewählt\n" << endl;
             break;
             case 3:
-                cout << "Sie haben Email versenden gewählt. \nAn wenn möchten sie ein Mail versenden?" << endl;
+                cout << "Sie haben Email versenden gewählt. \nAn wenn möchten sie ein Mail versenden?\n" << endl;
             break;
             case 4:
                 x = false;
+            break;
+            default:
+                cout << "Ungültige Eingabe\n" << endl;
             break;
         }
     }
