84c5518c3a1cc2158ac6cc4554163ca3b612f896
Latrusanimi <stephan.fankhauser@bbz-sh.ch>
2025-04-15 11:20:07 +0200

    Merge Versuch 1

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/kai0/OneDrive/Dokumente/GitHub/Projekt_Mailer
+# Install script for directory: C:/Users/steph/OneDrive - Berufsbildungszentrum Schaffhausen/Informatik/CLionProjects/Projekt_2025/Projekt_Mailer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.1.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.2.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/kai0/OneDrive/Dokumente/GitHub/Projekt_Mailer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/steph/OneDrive - Berufsbildungszentrum Schaffhausen/Informatik/CLionProjects/Projekt_2025/Projekt_Mailer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
