ecbb30599b60031cbcee689b190c60300e4f92e5
pesche70 <Peter.Meier@bbz-sh.ch>
2025-04-16 13:33:03 +0200

    6.20250616 -CSV-erstellen und füllen.(schreiben_csv()) -Datensatz auswählen aus CSV-Datei filtern_csv()   -Ausgabe Datensatz in einem String mit trennzeichen ";"   -in main hochladen   -string in main anzeigen.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ bool schreiben_csv(string mitgliedernummer,string nachname,string vorname,string
 
 string filtern_csv();
 
+/*
 string mitgliedernummer_;
 string nachname_;
 string vorname_;
@@ -37,17 +38,19 @@ int main()
     string wohnort="Schaffhausen";
     string typ="a";
     string telefonnummer = "052111222";
+    */
 
-   /*if (!schreiben_csv( mitgliedernummer,nachname,vorname,strasse, hausnummer,
+   if (!schreiben_csv( mitgliedernummer,nachname,vorname,strasse, hausnummer,
                         plz, wohnort, email, telefonnummer, geschlecht,typ))
-    {
+        {
         cerr << "Ein Fehler ist aufgetreten." << std::endl;
         return 1;
-    }
-*/
+        }
 
     string result =   filtern_csv();
-    cout << "Der String ist :1"<<result <<endl;
+/*
+cout << "Der String ist :"<<result <<endl;
+*/
     return 0;
 }//END main
 
@@ -63,7 +66,8 @@ bool schreiben_csv(string mitgliedernummer,string nachname,string vorname,string
     bool dateiExistiert = filesystem::exists(dateiname);
 
     ofstream datei(dateiname, std::ios::app);
-    if (!datei) {
+    if (!datei)
+    {
         cerr << "Fehler beim oeffnen der Datei!" << std::endl;
         return false;
     }
@@ -93,51 +97,52 @@ bool schreiben_csv(string mitgliedernummer,string nachname,string vorname,string
 }//END schreiben_csv()
 
 //Filtern mit auswahl und erstellen zusammengesetzten string
-string filtern_csv() {
+string filtern_csv()
+{
     string dateiname = "adresse.csv";
     ifstream datei(dateiname);
-    int i;
 
     if (!datei)
-    {
+        {
         cerr << "Datei konnte nicht geoeffnet werden.\n";
-        //return 0;
-    }
+        return ""; //Fehlerfall string zurückgeben
+        }
 
     vector<string> spalten =
-    {
+        {
         "Mitglied-Nr.", "Name", "Vorname", "Strasse", "Hausnummer",
         "PLZ", "Wohnort", "E-Mail", "Tel.Nr.", "Geschlecht", "Typ"
-    };
+        };
 
     cout << "\nNach welcher Spalte moechten Sie filtern?\n";
     for (size_t i = 0; i < spalten.size(); ++i)
-    {
+        {
         cout << i << ": " << spalten[i] << endl;
-    }
+        }
     int spaltenIndex;
     while (true)
-    {
+        {
         cout <<"Spaltenindex eingeben (0 bis " << spalten.size() - 1 << "): ";
         cin >> spaltenIndex;
 
         // Prüfen ob Eingabe überhaupt eine ganze Zahl ist
-        if (cin.fail()) {
+        if (cin.fail())
+            {
             cin.clear(); // Fehlerstatus zurücksetzen
             cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Puffer leeren
             cerr <<"Ungueltige Eingabe. Bitte eine ganze Zahl eingeben.\n";
             continue; // Zurück zum Anfang der Schleife
-        }
+            }
 
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Restliche Eingabe leeren
 
         // Prüfen ob Zahl im gültigen Bereich liegt
         if (spaltenIndex < 0 || spaltenIndex >= static_cast<int>(spalten.size()))
-        {
+            {
             cerr << "Ungueltiger Spaltenindex. Bitte einen Wert zwischen 0 und "
                  << spalten.size() - 1 << " eingeben.\n";
             continue;
-        }
+            }
         break; // Alles OK, Schleife verlassen
     }
     cout << "Suchbegriff eingeben: ";
@@ -152,81 +157,93 @@ string filtern_csv() {
 
     cout << "\n--- Gefundene Eintraege ---\n";
 
-    while (getline(datei, zeile)) {
-        if (ersteZeile)
+    while (getline(datei, zeile))
         {
+        if (ersteZeile)
+            {
             ersteZeile = false;
             continue;
-        }
+            }
 
         vector<string> felder;
         size_t pos = 0;
         while ((pos = zeile.find(';')) != string::npos)
-        {
+            {
             felder.push_back(zeile.substr(0, pos));
             zeile.erase(0, pos + 1);
-        }
+            }
         felder.push_back(zeile);
 
         if (spaltenIndex < felder.size() && felder[spaltenIndex].find(suchbegriff) != string::npos)
-        {
+            {
             treffer.push_back(felder);
             for (size_t i = 0; i < felder.size(); ++i)
-            {
+                {
                 cout << felder[i];
                 if (i < felder.size() - 1) cout << ";";
-            }
+                }
             cout << endl;
             gefunden = true;
+            }
         }
-    }
         if (!gefunden)
-        {
+            {
             cout << "Keine passenden Eintraege gefunden.\n";
-            //return 0;
+            return "";
+            }
+    //Falls genau ein Treffer besteht
+    if (treffer.size() == 1)
+        {
+        const auto& zeile = treffer[0];
+        string zusammenhang = zeile[0] + ";" + zeile[1] + ";" + zeile[2] + ";" +
+                               zeile[3] + ";" + zeile[4] + ";" + zeile[5] + ";" +
+                               zeile[6] + ";" + zeile[7] + ";" + zeile[8] + ";" +
+                               zeile[9] + ";" + zeile[10];
+
+        return zusammenhang;
         }
 
+    // Bei mehreren Treffern: Detailabfrage durchführen
+    if (treffer.size() > 1)
+        {
+        cout << "\nMehrere Eintraege gefunden.\n";
+        cout << "Bitte Mitgliedsnummer eingeben fuer Detailansicht: ";
+        string mitgliedsnr;
+        getline(cin, mitgliedsnr);
 
-
-            if (treffer.size() > 1)
+        bool detailGefunden = false;
+        for (const auto& zeile : treffer)
             {
-                cout << "\nMehrere Eintraege gefunden.\n";
-                cout << "Bitte Mitgliedsnummer eingeben fuer Detailansicht: ";
-                string mitgliedsnr;
-                getline(cin, mitgliedsnr);
-
-                bool detailGefunden = false;
-                for (const auto& zeile : treffer)
+            if (zeile[0] == mitgliedsnr)
                 {
-                    if (zeile[0] == mitgliedsnr)
-                    {
-                        // 🟢 Werte in Variablen abfüllen
-                        string mitgliedernummer_   = zeile[0];
-                        string nachname_           = zeile[1];
-                        string vorname_            = zeile[2];
-                        string strasse_            = zeile[3];
-                        string hausnummer_         = zeile[4];
-                        string plz_                = zeile[5];
-                        string wohnort_            = zeile[6];
-                        string  email_             = zeile[7];
-                        string telefonnummer_      = zeile[8];
-                        string geschlecht_         = zeile[9];
-                        string typ_                = zeile[10];
-                        cout<<mitgliedernummer_<<"tttt"<<endl;
-                        zusammenhang =  mitgliedernummer_ + " " + nachname_ + " "+vorname_ + " " + strasse_
-                        + " " + hausnummer_ + " "+ plz_ + " " + wohnort_ + " "+email_ + " " + telefonnummer_
-                        + " "+ geschlecht_ + " " + typ_ ;
+                string mitgliedernummer_ = zeile[0];
+                string nachname_         = zeile[1];
+                string vorname_          = zeile[2];
+                string strasse_          = zeile[3];
+                string hausnummer_       = zeile[4];
+                string plz_              = zeile[5];
+                string wohnort_          = zeile[6];
+                string email_            = zeile[7];
+                string telefonnummer_    = zeile[8];
+                string geschlecht_       = zeile[9];
+                string typ_              = zeile[10];
 
-                        detailGefunden = true;
-                        return zusammenhang;
-                        // break;
-                    }
-                }
-                if (!detailGefunden)
-                {
-                    cout << "Mitgliedsnummer nicht in der Auswahl gefunden.\n";
-                    //return 0;
+
+                //Zusammensetzen der Daten zu einem string
+                string zusammenhang = mitgliedernummer_ + ";" + nachname_ + ";" + vorname_ + ";" +
+                                      strasse_ + ";" + hausnummer_ + ";" + plz_ + ";" +
+                                      wohnort_ + ";" + email_ + ";" + telefonnummer_ + ";" +
+                                      geschlecht_ + ";" + typ_;
+                detailGefunden = true;
+                return zusammenhang;
                 }
             }
-
-    }// END filtern()
+        if (!detailGefunden)
+            {
+                    cout << "Mitgliedsnummer nicht in der Auswahl gefunden.\n";
+            return ""; // oder einen Hinweis-String zurückgeben
+            }
+        }
+    // Falls keine Treffer gefunden wurden (obwohl das schon abgefangen wurde)
+    return "";
+}// END filtern()
