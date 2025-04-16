6b2f037a376af1a2c5ad45126d66a30dcf0b71a8
Kai-z3 <Kai.Knezevic@bbz-sh.ch>
2025-04-16 13:19:44 +0200

    Update Passiv.cpp

--- Passiv.cpp
+++ Passiv.cpp
@@ -1,22 +1,19 @@
 //
 // Created by kai0 on 14/04/2025.
 //
-#include <iostream>
-#include <string>
-#include "Mitglieder.h"
-#include "Aktiv.h"
 #include "Passiv.h"
-#include "Ehrenmitglied.h"
 
-using namespace std;
-
-Passiv::Passiv(string telefon, string betrag)
-    : Mitglieder(telefon), Geldbetrag(betrag) {}
-
-
-void Passiv::Datenout() const {
-
-    cout << "Passiv Mitglied: " << Nachname << ", " << Vorname << ", Geschlecht: " << Geschlecht << ", Tel: " << Telnummer << ", Betrag: " << Geldbetrag <<", Mitgliedernr. "<< getMitgliedernummer() << endl;
+Passiv::Passiv(string telefon)
+    : Mitglieder(telefon) {
+    Geldbetrag = "30 Fr.";
 }
 
+void Passiv::Datenout() const {
+    cout << "Passivmitglied: " << Nachname << ", " << Vorname
+         << ", Geschlecht: " << Geschlecht
+         << ", Tel: " << getTelnummer()
+         << ", Betrag: " << getGeldbetrag()
+         << ", Mitgliedernr. " << getMitgliedernummer()
+         << endl;
+}
 
